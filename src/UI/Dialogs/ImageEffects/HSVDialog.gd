2126c5a48ff87a9ca95b4164ef57c4277ca7850b
Darshan Phaldesai <dev.darshanphaldesai@gmail.com>
2021-04-12 12:40:13 +0530

    HSV now works with selections and multi frames

--- src/UI/Dialogs/ImageEffects/HSVDialog.gd
+++ src/UI/Dialogs/ImageEffects/HSVDialog.gd
@@ -32,15 +32,23 @@ func _confirmed() -> void:
 
 func commit_action(_cel : Image, _project : Project = Global.current_project) -> void:
 	#DrawingAlgos.adjust_hsv(_cel, hue_slider.value, sat_slider.value, val_slider.value, selection_checkbox.pressed, _project)
+	var selection = Global.canvas.selection.get_big_bounding_image()
+	var selection_tex = ImageTexture.new()
+	selection_tex.create_from_image(selection)
+
 	if !confirmed:
 		$VBoxContainer/Preview.material.set_shader_param("hue_shift_amount", hue_slider.value /360)
 		$VBoxContainer/Preview.material.set_shader_param("sat_shift_amount", sat_slider.value /100)
 		$VBoxContainer/Preview.material.set_shader_param("val_shift_amount", val_slider.value /100)
+		$VBoxContainer/Preview.material.set_shader_param("selection", selection_tex)
+		$VBoxContainer/Preview.material.set_shader_param("affect_selection", selection_checkbox.pressed)
 	else:
 		var params = {
 			"hue_shift_amount": hue_slider.value /360,
 			"sat_shift_amount": sat_slider.value /100,
 			"val_shift_amount": val_slider.value /100,
+			"selection": selection_tex,
+			"affect_selection": selection_checkbox.pressed,
 		}
 		var gen: ShaderImageEffect = ShaderImageEffect.new()
 		gen.generate_image(_cel, "res://src/Shaders/HSV.shader", params)
